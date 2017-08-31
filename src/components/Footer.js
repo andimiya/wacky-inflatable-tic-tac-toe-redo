3a604ea8aa417547cd12a8429a96197458fa639c
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-30 21:03:21 -1000

    Homepage styled correctly and somewhat responsive, need to go back and add mobile styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-container-inner">
         <div className="footer-item">
-          <Link to="/">
-            <p>Home</p>
-          </Link>
-        </div>
-        <div className="footer-item">
-          <Link to="/articles">
-            <p>Articles</p>
-          </Link>
-        </div>
-        <div className="footer-item">
-          <Link to="/draftmanual">
-            <p>2017 Draft</p>
-          </Link>
-        </div>
-        <div className="footer-item">
-          <Link to="/about">
-            <p>About Us</p>
-          </Link>
-        </div>
-        <div className="footer-item">
-          <Link to="/contact">
-            <p>Contact Us</p>
-          </Link>
+            <p>Wacky Inflatable Tic-Tac-Toe</p>
         </div>
       </div>
     </div>
--- src/containers/HomepageContainer/index.js
+++ src/containers/HomepageContainer/index.js
@@ -2,28 +2,43 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import PageHeaderSubText from '../../components/PageHeaderSubText';
 import PageIntroText from '../../components/PageIntroText';
+import { Link } from 'react-router-dom';
 
+import title from '../../assets/title.png';
+import pandaBody from '../../assets/panda-body.png';
+import bunnyBody from '../../assets/bunny-body.png';
+import pandaRight from '../../assets/panda-right.png';
+import pandaLeft from '../../assets/panda-left.png';
+import bunnyRight from '../../assets/bunny-right.png';
+import bunnyLeft from '../../assets/bunny-left.png';
+import startButton from '../../assets/start.png';
 
 class HomepageContainer extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-
-    };
-  }
-
-  componentDidMount() {
   }
 
   render() {
     return (
-      <div className="homepage-container">
-        <div className="header-container-outer outer">
-          <div className="header-container div-container">
-            <PageHeader title="Better Odds of Football" />
-            <PageHeaderSubText text="Football - Stats - Blog" />
-            <PageIntroText text="See some of our rants below" />
+      <div className="homepage-container-outer">
+        <div className="homepage-container">
+          <div className="title-container">
+            <img className="title" src={title} />
+          </div>
+          <div className="animals-container">
+            <div className="panda-container">
+                <img className="panda-body" src={pandaBody} />
+                <img className="panda-right" src={pandaRight} />
+                <img className="panda-left" src={pandaLeft} />
+            </div>
+            <Link to="/play">
+              <img className="start-button" src={startButton} />
+            </Link>
+            <div className="bunny-container">
+              <img className="bunny-body" src={bunnyBody} />
+              <img className="bunny-right" src={bunnyRight} />
+              <img className="bunny-left" src={bunnyLeft} />
+            </div>
           </div>
         </div>
       </div>
