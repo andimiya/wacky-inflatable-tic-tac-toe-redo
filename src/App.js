5310d6cdbbd1761e426e2ba7e39b092dd62db59b
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-30 21:05:15 -1000

    Removed footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomepageContainer from './containers/HomepageContainer';
 import GameContainer from './containers/GameContainer';
-import Footer from './components/Footer';
 
 const App = () =>
   <div id="app-container">
@@ -10,7 +9,6 @@ const App = () =>
       <div className="app">
         <Route exact path="/" component={HomepageContainer} />
         <Route exact path="/play" component={GameContainer} />
-        <Footer />
       </div>
     </Router>
   </div>;
